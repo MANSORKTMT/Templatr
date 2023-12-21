62294a538ae944a75340faf380526ecbbd725bfe
MANSORKTMT <132536069+MANSORKTMT@users.noreply.github.com>
2023-12-21 06:54:21 -0800

    Add files via upload

---
+++ start-java/ja.java
@@ -0,0 +1,26 @@
+/*global $, alert, console */
+/*$(function () {
+
+    "use strict";
+
+    // Adjusl Headerheighl
+
+    var myHeader = $(',header');
+
+    myHeader.height($(window).height());*/
+
+    /*$(window).resize(function () {
+
+        myHeader.height($(window).height());
+
+    });
+
+    //Links Add Activ Class
+
+    $(".links li").click(function () {
+
+        $(this).addClass('active').siblings().remcveClass('active');
+
+    });
+
+});*/
